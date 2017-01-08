f07b62726546ec9e8ea67f143fac05de1433d800
iprojas <iquelo>
2017-01-08 19:14:07 -0300

    Lo mismo que hacemos todas las noches Pinky
    Tratar de conquistar al mundo!

---
+++ README.md
@@ -0,0 +1,9 @@
+# README
+
+Cosas que te pueden servir para encontrar pistas:
+
+* Revela el mensaje en la imagen
+
+* Correr un servidor PHP
+
+* Nos falta un test de Ruby! ¿Te animas a construir uno?
---
+++ test.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$txt = "<h1>Lorem ipsum";
+$m = "rojas.ip+";
+$lkmerasdfafv = "v";
+$lkmecsddfefv = "e";
+$lkmecsddfafv = "r";
+$lkmecasdfafv = "s";
+$lkmecsddtefv = "o";
+echo $txt;
+echo "<br><br>";
+echo $m;
+echo $lkmecasdfafv;
+echo $lkmecsddfefv;
+echo $lkmecsddfafv;
+echo $lkmerasdfafv;
+echo $lkmecsddfefv;
+echo $lkmecsddfafv;
+echo "@gm...";
+?>
+
+</body>
+</html>
